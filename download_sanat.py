efc9a391a5b27f45945bf4f392e4ffe438c79f3c
rueter <rueter.jack@gmail.com>
2020-03-31 09:50:06 +0300

    @mikahama  Using print() in fin

--- download_sanat.py
+++ download_sanat.py
@@ -43,9 +43,9 @@ def download_xmls():
 	files = list_xmls()
 	for folder in files.keys():
 		for file in files[folder]:
-			print "Downloading: " + folder + ", " + file
+			print("Downloading: " + folder + ", " + file)
 			payload = {'language': language, 'file': file, "type": folder}
-			r = requests.get('http://sanat.csc.fi:8000/smsxml/xml_out/', params=payload)
+			r = requests.get('http://www.akusanat.com/smsxml/xml_out/', params=payload)
 			if r.status_code != requests.codes.ok:
 				r.raise_for_status()
 			f = codecs.open(folder + "/" + file, "w", encoding="UTF-8")
@@ -56,20 +56,20 @@ def pretty_xmls():
 	files = list_xmls()
 	for folder in files.keys():
 		for file in files[folder]:
-			print "Format XML: " + folder + ", " + file
+			print("Format XML: " + folder + ", " + file)
 			pretty_xml(folder + "/" + file, folder + "/" + file)
 
 def change_branch():
-	print subprocess.check_output("git checkout -B wiki_branch" , shell=True, stderr=subprocess.STDOUT)
+	print(subprocess.check_output("git checkout -B wiki_branch" , shell=True, stderr=subprocess.STDOUT))
 
 
 
-print "Changing branch"
+print("Changing branch")
 change_branch()
-print "Downloading XMLs from sanat"
+print("Downloading XMLs from sanat")
 download_xmls()
 pretty_xmls()
-print "\n\nAll done!\n\nMerge wiki_branch to master. Push and go to http://sanat.csc.fi/smsxml/git_postmerge/?language=" + language
+print("\n\nAll done!\n\nMerge wiki_branch to master. Push and go to http://www.akusanat.com/smsxml/git_postmerge/?language=" + language)
 
 
 	 
